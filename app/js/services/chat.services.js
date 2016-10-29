2cce9bf761181f6b1f2e706212800788552b5dc6
Maxim Zorich <zorichmaxim@gmail.com>
2016-10-29 17:56:02 +0300

    Update chat.services.js

--- app/js/services/chat.services.js
+++ app/js/services/chat.services.js
@@ -1,6 +1,9 @@
 /**
  * Created by 123 on 20.10.2016.
  */
+    angular.module('cbsChat')
+        .factory('ChatService', ["$firebaseArray",chatService]);    
+  
     function chatService($firebaseArray) {
         var messagesRef = firebase.database().ref().child("messages");
         var chat = {};
@@ -8,18 +11,11 @@
         chat.getMessages = function(){
             return $firebaseArray(messagesRef);
         };
-
         chat.showMessages = function(){
             return $firebaseArray(messagesRef.limitToLast(15));
         };
         chat.sendMessage = function(message){
             chat.getMessages().$add(message);
         };
-
-
-
-
         return chat
 }
-    angular.module('cbsChat')
-        .factory('ChatService', ["$firebaseArray",chatService]);
