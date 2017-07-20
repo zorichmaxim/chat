7d659516d386b6035cc06c0c9205bc4a50520783
Maxim Zorich <zorichmaxim@gmail.com>
2017-07-20 10:31:27 +0300

    Update chat.services.js

--- app/js/services/chat.services.js
+++ app/js/services/chat.services.js
@@ -1,13 +1,9 @@
-/**
- * Created by 123 on 20.10.2016.
- */
     angular.module('cbsChat')
         .factory('ChatService', ["$firebaseArray",chatService]);    
   
     function chatService($firebaseArray) {
         var messagesRef = firebase.database().ref().child("messages");
         var chat = {};
-// разобрать смысл messagesRef - на сайте по firebase в документации
         chat.getMessages = function(){
             return $firebaseArray(messagesRef);
         };
